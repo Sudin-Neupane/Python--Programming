69877afbe196d9edaf7decf8971e51f1da843fc2
SuDin neUpaNe <sudinneupane519@gmail.com>
2026-06-23 14:00:43 +0545

    adding some codes

--- np array.py
+++ np array.py
@@ -1,6 +1,7 @@
 #create array of length 5
 import numpy as np
 
+
 data=np.zeros(5)
 print(data)
 #create array of length 5
@@ -17,4 +18,10 @@ data=np.empty(10)
 print(data)
 #create array of shape(2,3)
 data=np.empty((2,3))
+print(data)
+data=np.arange(10)
+print(data)
+data=np.full((3,3), 5)
+print(data)
+data = np.identity(3)
 print(data)
