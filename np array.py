f2e61e549d4d161d9fd33e404f5110441670e828
SuDin neUpaNe <sudinneupane519@gmail.com>
2026-06-23 13:48:17 +0545

    adding python file

---
+++ np array.py
@@ -0,0 +1,20 @@
+#create array of length 5
+import numpy as np
+
+data=np.zeros(5)
+print(data)
+#create array of length 5
+data=np.ones(5)
+print(data)
+#create array of shape(3,3)
+data=np.zeros((3,3))
+print(data)
+#create array of shape(3,3)
+data=np.ones((3,3))
+print(data)
+#create array of length 10
+data=np.empty(10)
+print(data)
+#create array of shape(2,3)
+data=np.empty((2,3))
+print(data)
