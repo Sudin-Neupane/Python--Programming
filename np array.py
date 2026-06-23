59beedc442bd61f3f124d4143414640f2093ddca
SuDin neUpaNe <sudinneupane519@gmail.com>
2026-06-23 21:52:29 +0545

    adding extra codes

--- np array.py
+++ np array.py
@@ -1,7 +1,5 @@
 #create array of length 5
 import numpy as np
-
-
 data=np.zeros(5)
 print(data)
 #create array of length 5
@@ -21,7 +19,22 @@ data=np.empty((2,3))
 print(data)
 data=np.arange(10)
 print(data)
-data=np.full((3,3), 5)
+data=np.full((3,3), 8)
 print(data)
 data = np.identity(3)
 print(data)
+#create array with elements 0-9
+data=np.arange(10)
+print(data)
+#create array with elements 5-9
+data=np.arange(5,10)
+print(data)
+#create array with elements 1-9
+data=np.arange(4,10,2)
+print(data)
+#create array with elements 0-9
+data=np.arange(10)
+print(data)
+d=np.ones_like(data)
+print(d)
+
