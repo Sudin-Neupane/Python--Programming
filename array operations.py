d9e7e768d4ee9cd7aba520f6e6134de4b07fdf11
SuDin neUpaNe <sudinneupane519@gmail.com>
2026-06-25 13:51:53 +0545

    adding python for array operations

--- array operations.py
+++ array operations.py
@@ -1,9 +1,32 @@
 import numpy as np
-a = np.array([[1., 2., 3.], [4., 5., 6.]])
+
+a = np.array([1., 2., 3., 4., 5., 6.])
 print(a)
-r=a*a
+
+r = a * a
 print("Element-wise multiplication of arrays:")
 print(r)
-r=a+a
+
+r = a + a
 print("Sum of arrays:")
 print(r)
+
+r = a / 2
+print("Half of array elements:")
+print(r)
+
+r = a**0.5
+print("Square root of array elements:")
+print(r)
+
+print("Array Elements:")
+print(a)
+print("Element at index 3")
+print(a[3])
+print("Element from index 3 to 5")
+print(a[3:6])
+
+a[2] = 10  # modifying element at index 2
+a[4:6] = 11  # modifying elements from index 4 to 5
+print("Array Elements:")
+print(a)
