4c5f62d39e9432f07fb35d546a455d77cea434fe
SuDin neUpaNe <sudinneupane519@gmail.com>
2026-06-25 13:46:27 +0545

    adding python for array operations

---
+++ array operations.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([[1., 2., 3.], [4., 5., 6.]])
+print(a)
+r=a*a
+print("Element-wise multiplication of arrays:")
+print(r)
+r=a+a
+print("Sum of arrays:")
+print(r)
--- exception_2.py
+++ exception_2.py
@@ -42,3 +42,4 @@ if __name__ == "__main__":
 
 	c = divide(x, y)
 	print(c)
+	
