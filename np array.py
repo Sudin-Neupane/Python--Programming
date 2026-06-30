a296242beed8b77284cb163bb496afc7b47faaeb
SuDin neUpaNe <sudinneupane519@gmail.com>
2026-06-30 22:49:27 +0545

    adding py

--- np array.py
+++ np array.py
@@ -64,3 +64,18 @@ print(data)
 data=data.astype('float64') #Error
 print(data)
 print(data.dtype)
+
+a = np.arange(10)
+print("Dataset:",a)
+s=np.sqrt(a)#unary universal function
+print("Square Roots:",s)
+e=np.exp(a)
+print("Exp(a):",e)
+x=np.random.randn(10)
+y=np.random.randn(10)
+z=np.maximum(x,y)#bunary universal function
+print("x=",x)
+print("y=",y)
+print("z=",z)
+m=np.max(x)
+print("Maximum=",m)
